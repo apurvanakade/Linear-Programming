19be7d0edc8c95c0b1c35cbbbf6e08837a2da30c
Apurva Nakade <apurvnakade@gmail.com>
2021-06-09 13:18:29 -0400

    Finalize simplex method

--- simplex.py
+++ simplex.py
@@ -17,29 +17,33 @@ class LinearProgram:
     """
 
     def __init__(self, A: np.array, b: np.array, cost: np.array):
-        if len(A[0]) != len(cost):
-            # TODO: improve the type check here
+
+        self.num_eq = len(A)  # number of equations
+        self.num_var = len(A[0])  # number of variables
+
+        if len(cost) != self.num_var:
             print(
                 "Error: Number of rows of A does not match\
                 the number of rows of the cost function."
             )
             return
-        if len(A) != len(b):
-            # TODO: improve the type check here
+        if len(b) != self.num_eq:
             print("Error: Number of rows of A does not match the number of rows of b.")
             return
+
         self.A = np.array(A)
         self.b = np.array(b)
         self.cost = np.array(cost)
-        self.num_eq = len(A)  # number of equations
-        self.num_var = len(A[0])  # number of variables
 
     def print(self):
         "Prints the system of equations."
 
-        print("Minimize: " + str(self.cost))
-        print("Subject to constraints:")
+        print("Minimize: ")
+        for i in range(self.num_var):
+            print(str(self.cost[i]) + "*x_" + str(i), end=" + ")
+        print(str(self.cost[self.num_var - 1]) + "*x_" + str(self.num_var - 1))
 
+        print("Subject to constraints:")
         for i in range(self.num_eq):
             for j in range(self.num_var - 1):
                 print(str(self.A[i][j]) + "*x_" + str(j), end=" + ")
@@ -66,28 +70,29 @@ class SimplexMethod:
     """
 
     def __init__(self, lp: LinearProgram, basis_columns: np.array):
+
         self.lp = lp
         self.basis_columns = np.array(basis_columns)
-        # self.vertex = np.array(starting_vertex)
-
         self.update_inv_naive()
-        self.vertex = np.zeros(self.lp.num_var)
         self.calculate_vertex()
-
-        self.print()
 
     def update_inv_naive(self):
         self.B_inv = np.linalg.inv(self.lp.A[:, self.basis_columns])
 
     def calculate_vertex(self):
+
         self.vertex = np.zeros(self.lp.num_var)
-        temp = np.matmul(self.B_inv, self.lp.b)
+
+        # aux_vertex = B^{-1} * b
+        aux_vertex = np.matmul(self.B_inv, self.lp.b)
+
+        # put the values of `aux_vertex` into appropriate "spots".
         for i in range(self.lp.num_var):
             if i in self.basis_columns:
-                self.vertex[i] = temp[(np.where(self.basis_columns == i))[0]]
+                self.vertex[i] = aux_vertex[(np.where(self.basis_columns == i))[0]]
 
-    def cost(self, vector: np.array):
-        return np.matmul(np.transpose(self.cost), vector)
+    def cost(self):
+        return np.dot(self.lp.cost, self.vertex)
 
     def reduced_cost_naive(self, index: int):
         "Returns the reduced cost in the direction `j`: c'_j = c_j - c'_B * B^{-1} * A_j"
@@ -101,34 +106,30 @@ class SimplexMethod:
 
         # u = B^{-1} * A_i
         u = np.matmul(self.B_inv, self.lp.A[:, i])
-        print("Mystery vector:", u)
 
         # values in `u` that are positive
-        pos_val = u[u > 0]
-        print("Postive entries: ", pos_val)
+        u_pos = u[u > 0]
 
-        b_pos = self.basis_columns[u > 0]
-        print("Corresponding columns: ", b_pos)
-
-        # coordinates of `x` corresponding to the positive values of `u`
-        x = self.vertex[b_pos]
-        print("Positive vertex coords:", x)
-
-        # If no-coordinates of `x` are positive,
+        # If no-coordinates of `u` are positive,
         # then there is no vertex in this direction.
-        if len(pos_val) == 0:
+        if len(u_pos) == 0:
             return False
 
+        # Basis vectors corresponding to the positive entries in `u`
+        b_pos = self.basis_columns[u > 0]
+
+        # Coordinates of `x` corresponding to the positive values of `u`
+        x = self.vertex[b_pos]
+
         # normalize the positive coordinates and find the min values
-        thetas = x / pos_val
+        thetas = x / u_pos
         theta_argmin = np.argmin(thetas)
         theta_min = min(thetas)
-        print("thetas, argmin, min: ", thetas, theta_argmin, theta_min)
 
         # Replace the i^th basis_column with the `theta_argmin` one
         self.basis_columns[theta_argmin] = i
         self.update_inv_naive()
-        print("new basis: ", self.basis_columns)
+        self.calculate_vertex()
 
         # Coordinates of the new vertex
         # self.vertex = [
@@ -144,28 +145,15 @@ class SimplexMethod:
         # new_vertex[self.basis_columns[theta_argmin]] = theta_min
         # self.vertex = new_vertex
 
-        B = [self.lp.A[:, i] for i in self.basis_columns]
-        print("new B:", B)
-        print("Columns of A:")
-        print(self.lp.A[:, self.basis_columns])
-        print("new B_inv:")
-        print(self.B_inv)
-        self.calculate_vertex()
-
-        print("new vertex: ", self.vertex)
         return True
 
     def solve(self, reduced_cost_calc=reduced_cost_naive):
         # Loop over the indices `i` which are not in one of the basis_columns.
         for i in range(self.lp.num_var):
-            print("Testing direction ", i)
-            input("Press any key to continue...")
             if not (i in self.basis_columns):
-                print(i, "not in basis_columns")
 
                 # Calculate the reduced cost in the `i` direction.
                 reduced_cost = self.reduced_cost_naive(i)
-                print("reduced cost: ", reduced_cost)
 
                 # If the reduced cost is negative,
                 # then move in the direction of `i` to the next vertex.
@@ -176,23 +164,24 @@ class SimplexMethod:
                     # else the cost is -infinity
                     if self.step(i):
                         break
-                    print("Cost is -infinite")
                     return -1
 
                 # If all reduced costs are non-negative
                 # then `i` is the optimal solution
-                print("Found optimal solution: ", self.vertex)
-                # print("Optimal cost: ", self.cost(self.vertex))
                 return i
 
         self.solve()
 
-    def print(self):
+    def print_solution(self):
+
+        print("-" * 20)
         self.lp.print()
-        print("Basis columns: ", self.basis_columns)
-        print("Starting vertex: ", self.vertex)
-        print("Basis inverse:")
-        print(self.B_inv)
+
+        print("-" * 20)
+        print("Solution:")
+        for i in range(len(self.vertex)):
+            print("x_" + str(i) + " = " + str(round(self.vertex[i], 2)))
+        print("Minimal cost = ", round(self.cost(), 2))
         return
 
 
@@ -205,6 +194,8 @@ lp = LinearProgram(
 simplex = SimplexMethod(lp, basis_columns=[3, 4, 5])
 
 simplex.solve()
+
+simplex.print_solution()
 
 # lp.print()
 
